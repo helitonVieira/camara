55fe40a8505c7c1a1c14443d0ff43fee267930e9
Heliton Silva Vieira <heliton.vieira@gmail.com>
2025-07-25 12:32:35 -0300

    Projeto criado

---
+++ src/main/java/com/jh/CamaraBackApplication.java
@@ -0,0 +1,13 @@
+package com.jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamaraBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamaraBackApplication.class, args);
+	}
+
+}
