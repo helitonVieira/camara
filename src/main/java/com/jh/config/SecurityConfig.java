a973164e77116fe8b73e4eb4e700cf37171966ca
Heliton Silva Vieira <heliton.vieira@gmail.com>
2025-07-31 19:28:00 -0300

    liberado get municipio

--- src/main/java/com/jh/config/SecurityConfig.java
+++ src/main/java/com/jh/config/SecurityConfig.java
@@ -42,8 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
-			"/projeto/**"/*,
-			"/categorias/**",
+			"/projeto/**",
+			"/municipio/**"/*,
 			"/estados/**"*/
 	};
 
